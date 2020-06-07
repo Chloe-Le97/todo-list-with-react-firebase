dd0670407fe099bf81d09744ae1ba1206243ae43
cat-isme <phuonganh1902.le@gmail.com>
2020-06-07 08:55:09 +0700

    Updates

---
+++ precache-manifest.3aa3e6beb3dae5c13f0f22f60911fae7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eea03a94476fa458b602eb4f8dfcaf5",
+    "url": "/todo-list-with-react-firebase/index.html"
+  },
+  {
+    "revision": "1c706245e439ffd77190",
+    "url": "/todo-list-with-react-firebase/static/css/main.c07c3f75.chunk.css"
+  },
+  {
+    "revision": "65cb6b2db06fbe7a615c",
+    "url": "/todo-list-with-react-firebase/static/js/2.5507e45a.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/todo-list-with-react-firebase/static/js/2.5507e45a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c706245e439ffd77190",
+    "url": "/todo-list-with-react-firebase/static/js/main.764aa066.chunk.js"
+  },
+  {
+    "revision": "38e22fcbe3ca9a18f9fd",
+    "url": "/todo-list-with-react-firebase/static/js/runtime-main.ffe9eda1.js"
+  }
+]);
